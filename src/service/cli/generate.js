601cfcc498a5d783f92caa6e5918dd2690b1921d
andpost200 <andrey.chepulis@kreo.net>
2021-07-29 11:30:00 +0300

    task2

---
+++ src/service/cli/generate.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const fs = require(`fs`);
+const util = require(`util`);
+const {exit} = require(`process`);
+const chalk = require(`chalk`);
+
+const {
+  getRandomInt,
+  shuffle,
+} = require(`../../utils`);
+
+
+const DEFAULT_COUNT = 1;
+const FILE_NAME = `mocks.json`;
+
+const OfferType = {
+  OFFER: `offer`,
+  SALE: `sale`,
+};
+
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+
+const SumRestrict = {
+  MIN: 1000,
+  MAX: 100000,
+};
+
+const PictureRestrict = {
+  MIN: 1,
+  MAX: 16,
+};
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const readContent = async (filePath) => {
+  try {
+    const content = await readFileAsync(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, categories, sentences) => (
+  Array(count).fill({}).map(() => ({
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(1, 5).join(` `),
+    picture: getRandomInt(PictureRestrict.min, PictureRestrict.max),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
+    sum: getRandomInt(SumRestrict.min, SumRestrict.max),
+  }))
+);
+
+module.exports = {
+  name: `--generate`,
+  async run(args) {
+
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const content = JSON.stringify(generateOffers(count, titles, categories, sentences));
+    if (countOffer > 1000) {
+      console.info(chalk.yellow(`Не больше 1000 публикаций`));
+      exit();
+    }
+
+    try {
+      writeFileAsync(FILE_NAME, content);
+      console.info(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
+  }
+};
