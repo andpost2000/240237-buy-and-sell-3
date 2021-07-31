0f9aec30a94fea85a449ef8d9f71cf5942894da5
andpost200 <andrey.chepulis@kreo.net>
2021-07-31 23:01:51 +0300

    fix

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -6,8 +6,8 @@ const offersRouter = new Router();
 
 offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id ${req.params.id}`));
 offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id ${req.params.id}`));
-offersRouter.put(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id ${req.params.id}`));
-offersRouter.post(`/add`, (req, res) => res.send(`/offers/add`));
+offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id ${req.params.id}`));
+offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
 
 
 module.exports = offersRouter;
